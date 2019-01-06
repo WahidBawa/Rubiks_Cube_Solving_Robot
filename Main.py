1058dffdd4857440463544baf93825eb8a100a6c
Wahid Bawa <wahidbawa10@gmail.com>
2019-01-06 12:26:58 -0500

    added comments and made everything test ready

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 import sys
-from rubik_solver import utils
-import serial
+from rubik_solver import utils # this is how we actually get the algorithm for solving the cube
+import serial # this will be used so that the python interface can interact with the arduino interface
+
+# these are just a bunch of scrambles and the cube states along side them
 # Scramble: D' B2 L U' B L' D R' D' B2 U2 F' R2 L2 B' L2 B U2 L2 #green on front; white on top
 # cube = 'oyrryorwwyyybbrywoboogrogwrggbygygrowbbgoobwrybwgwbgrw' #solve with red on front; yellow on top
 
 # Scramble: R' B' U' B U' B2 L' B' U' B2 U2 F2 L2 U2 D' R2 L2 B2 L2 R' #green on front; white on top
-# cube = 'gboyygygrygowbywbybwwrroryggrwwgyrowbrooorbwobbyowggbr' #solve with red on front; yellow on top
+cube = 'gboyygygrygowbywbybwwrroryggrwwgyrowbrooorbwobbyowggbr' #solve with red on front; yellow on top
 
 # Scramble: F2 L2 F2 D U2 L2 B2 D' B2 F2 U2 F' R' B2 F L' U' F2 U2 R' #green on front; white on top
 # cube = 'rwbyybbwrggoybboyywobwrroogworyggrrgygwrooorbggybwbyww' #solve with red on front; yellow on top
 
-cube = 'ryobybrgygwbgbrgbowwggrybowoybrgwgrryowroobgywwoowbryy' #solve with red on front; yellow on top
+# this below is just to count the amount of tiles with the same colour to ensure that their is no human error involved
 w = y = b = g = r = o = 0
 for i in cube:
 	if i == "w":
@@ -32,15 +34,15 @@ print("green: ", g)
 print("red: ", r)
 print("orange: ", o)
 
-solve = utils.solve(cube, 'Kociemba')
+solve = utils.solve(cube, 'Kociemba') # this gets a solve alg
 
-algo = []
-for i in solve:
-	tmp = str(i)
-	if len(tmp) == 1:
+algo = [] # this creates an empty list
+for i in solve: #this will go through your solve
+	tmp = str(i) # it converts the solve instance to a string
+	if len(tmp) == 1: # this will add a space on single clockwise moves
 		tmp += " "
-	algo.append(tmp)
-print(algo, "size:", len(algo))
+	algo.append(tmp) # this will add the single move to the algo list
+print(algo, "size:", len(algo)) # this will print out the solve along with the actual size of the moves
 
 
 # you need to give the arduino side of things to properly initialize from prior experience, can't be sure of this until we test the theory
--- Rubik_Solver/Rubik_Solver.ino
+++ Rubik_Solver/Rubik_Solver.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <Servo.h> // imports the library for the servos
 // the servos are made in the order that the colours are scanned
 Servo yellow;
 Servo blue;
@@ -8,8 +8,9 @@ Servo orange;
 Servo white;
 
 char SerialData; // this will store the next move to be made
-String str;
+String str; // this will be used later to convert the char to string
 void setup() {
+  // this is just attaching the servos to their respective pins
   yellow.attach(A0);
   blue.attach(A1);
   red.attach(A2);
@@ -23,17 +24,18 @@ void loop() {
    * Serial.available(); this will be checking whether there is input left
    * Serial.read(); this will read the input, the above line will make sure that there is somthing left to read to prevent any errors. This will be stored as a char
    */
-  if (Serial.available() > 0){
-    SerialData = Serial.read();   
-    str = SerialData + "";
-    turn(str.charAt(0), str.charAt(1)); 
+  if (Serial.available() > 0){ // this will check if their is any more input left to take from the serial port which is written to with the python code
+    SerialData = Serial.read(); // this will actually read the input from the serial port assuming that their is actual input to be read
+    str = SerialData + ""; // this just converts the char type variable to a string type variable
+//    turn(str.charAt(0), str.charAt(1)); // this will take in the side that needs to be turned and also the direction of the turn
+    Serial.println(str); //this is for testing purposes to see the actual input from the python side of things
   }
 }
 
 void turn(String side, String dir){
-  String sides = "ULFRBD";
-  Servo servos[] = {yellow, blue, red, green, orange, white};
-  int sideIndex = sides.indexOf(side);
+  String sides = "ULFRBD"; // this will be used to determine the index of the servos depending on which side needs to be turned
+  Servo servos[] = {yellow, blue, red, green, orange, white}; // this is just a simple list that stores the servos that are to be turned
+  int sideIndex = sides.indexOf(side); // this will get the index of the side that needs to be turned, this is basically the index we will use to determine which servo will be used
   if (dir == "2"){ // this will be for a double turn
     servos[sideIndex].write(180); // this will probably have to be adjusted later
     delay(500); //this will probably have to be adjusted later
